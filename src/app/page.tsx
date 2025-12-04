49d7839c20ce8928d1729610ef7bc40fc5e1d717
Joel Vargas <vermudo.com@gmail.com>
2025-12-04 13:03:25 -0600

    fix: build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import MainTemplate from "@/components/templates/main-template";
 import HeroSection from "@/components/organisms/hero-section";
 import FeaturesSection from "@/components/organisms/features-section";
 import HowItWorksSection from "@/components/organisms/how-it-works-section";
-import CallToActionSection from "@/components/organisms/call-to-action-section";
 import { Footer } from "@/components/organisms/footer";
 import Testimonials from "@/components/testimonials-section";
 import BenefitsSection from "@/components/organisms/benefits-section";
